dd7bdb0906a5c4b48cf654cdf79d2b6b2fc4ceeb
Valeria Pacheco <vpachecosanmartin@gmail.com>
2024-07-25 21:24:03 -0500

    "prueba" Realize los ejercicios

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,209 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    //Casteo
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    //Ejercicio 1
+    public static int calcularResultadoDex() {
+        int x = (int) 5.89 + (int) 3.45;
+        return x;
+    }
+    //Ejercicio 2
+    public static int calcularValorAscii() {
+        char c = 'A';
+        int ascii = (int) c;
+        return ascii;
+    }
+    //Ejercicio 3
+    public static int imprimirValorDei() {
+        double d = 100.04;
+        long l = (long)d;
+        int i = (int)l;
+        return i;
+    }
+    //Ejercicio 4
+    public static int convertirStringAint() {
+        String valor = "1456";
+        int resultadoConversion = Integer.parseInt(valor);
+        return resultadoConversion;
+    }
+    //Ejercicio 5
+    public static int resultadoDeConvercion() {
+        int resultado = (int) (char) (byte) -1;
+        return resultado;
+    }
+    //Ejercicio 6
+    public static int convertirNumeroEnChar() {
+        int numero = 65;
+        char cambiar = (char) numero;
+        return cambiar;
+    }
+    //Ejercicio 7
+    public static int asignarLongAInt() {   // Sin un casteo explícito vota error (int)
+        long valorLong = 123456789L;
+        int valorInt = (int) valorLong;
+        return valorInt;
+    }
+    //Ejercicio 8
+    public static String convertirFloatAString() {
+        float valorFloat = 3.14f;
+        String valorString = Float.toString(valorFloat);
+        return valorString;
+    }
+    //Ejercicio 9
+    public static int resultadoDeIntConMathRound() {
+        int resultadoDe = (int) Math.round(3.7);
+        return resultadoDe;
+    }
+    //Ejercicio 10
+    public  static short convertirByteAShort() {
+        byte b = 50;
+        short valorShort = b;
+        return valorShort;
+    }
+
+
+    //Operadores de asignacion
+    //Ejercicio 1
+    public static int valorX() {
+        int x = 10;
+        x += 5;
+        return x;
+    }
+    //Ejercicio 2
+    public static int valorY() {
+        int y = 5;
+        int x = 7;
+        y *= (x + 5);
+        return y;
+    }
+    //Ejercicio 3
+    public static int valorDea(){
+        int a = 15;
+        a %= 4;
+        return a;
+    }
+    //Ejercicio 4
+    public static int valorDeXUsandoOperadorDeAsignacionCompuesta(){
+        int x = 25;
+        int y = 19;
+         x += y;
+        return x;
+    }
+    //Ejercicio 5
+    public static int resultadoOperacion(){
+        int x = 4;
+        x ^= 2;
+        return x;
+    }
+
+
+    //Operadores de Incremento y Decremento
+    //Ejercicio 1
+    public static int resultadoDeXyY(){
+        int x = 5;
+        int y = ++x;
+        return y;
+    }
+    //Ejercicio 2
+    public static int resultadoDeAMas(){
+        int a = 5;
+        a++;
+        return a;
+    }
+    public static int resultadoDeA(){
+        int a = 5;
+        return a;
+    }
+    //Ejercicio 3
+    public static int resultadoDeDecremento(){
+        int count = 1;
+        count--;
+        return count;
+    }
+    //Ejercicio 4
+    public static int incrementoEnPrefijo(){
+        int i = 5;
+        ++i;
+        return i;
+    }
+    public static int incrementoEnSufijo(){
+        int i = 5;
+        i++;
+        return i;
+    }
+    //Ejercicio 5
+    public static int valordex (){
+        int x = 3;
+        x = x++;
+        return x;
+    }
+
+
+    //Ejercicios Combinados
+    //Ejercicio 1
+    public static int resultadoDeI(){
+        int i = 5;
+        i += ++i + i++ + ++i;
+        return i;
+    }
+    //Ejercicio 2
+    public static int convertirDoubleEnIntEincrementar(){
+        double d = 10.99;
+        int convertedInt = (int) d;
+        convertedInt++;
+        return convertedInt;
+    }
+    //Ejercicio 3
+    public static int valorDei(){
+        double d = 5.7;
+        int i = (int)d;
+        i *= 2;
+        i--;
+        return i;
+    }
+    //Ejercicio 4
+    public static double valorDeC() {
+        int v = 10;
+        double e = v /= 2.0;
+        return e;
+    }
+    //Ejercicio 5
+    public static int resultadoDeC(){
+        char c = 'X';
+        c += 32;
+        return c;
+    }
+    //Ejercicio 6
+    public static int convertirLongEnShort() {
+        long v = 5000L;
+        short g = (short) v;
+        g *= 3;
+        return g;
+    }
+    //Ejercicio 7
+    public static int resultadoOpercaionX(){
+        int x = 10;
+        x += (x++) + (++x);
+        return x;
+    }
+    //Ejercicio 8
+    public static int resultadoDelPredecremento(){
+        float numeroFloat = 123.45f;
+        byte numeroByte = (byte) numeroFloat;
+        --numeroByte;
+        return numeroByte;
+    }
+    //Ejercicio 9
+    public static int resultadoOperacionb(){
+        int i = 257;
+        byte b = (byte)i;
+        return b;
+    }
+    //Ejercicio 10
+    public static int resultadoDeIncrementarleEn1() {
+        double numeroDouble = 45.67;
+        int numeroInt = (int) numeroDouble;
+        numeroInt += 5;
+        numeroInt++;
+        return  numeroInt;
     }
 }
--- src/Solve.java
+++ src/Solve.java
@@ -1,2 +1,238 @@
+import java.util.Scanner;
+
 public class Solve {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcionPrincipal;
+
+        do {
+            System.out.println("===== Menú Principal =====");
+            System.out.println("1. Casteo y Operaciones");
+            System.out.println("2. Operadores de Asignación");
+            System.out.println("3. Operadores de Incremento y Decremento");
+            System.out.println("4. Ejercicios Combinados");
+            System.out.println("0. Salir");
+            System.out.println("Seleccione una opcion: ");
+            opcionPrincipal = scanner.nextInt();
+
+            switch (opcionPrincipal) {
+                case 1:
+                    submenuCasteoYOperaciones(scanner);
+                    break;
+                case 2:
+                    submenuOperadoresDeAsignacion(scanner);
+                    break;
+                case 3:
+                    submenuOperadoresDeIncrementoYDecremento(scanner);
+                    break;
+                case 4:
+                    submenuEjerciciosCombinados(scanner);
+                    break;
+                case 0:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intentelo nuevamente.");
+            }
+        } while (opcionPrincipal != 0);
+
+        scanner.close();
+
+    }
+
+    private static void submenuCasteoYOperaciones(Scanner scanner) {
+        int option;
+        do {
+            System.out.println("Submenú Casteo y Operadores:");
+            System.out.println("1. Resultado de (int) 5.89 + (int) 3.45");
+            System.out.println("2. Resultado del valor ASCII de 'A'");
+            System.out.println("3. Resultado del valor double 100.04");
+            System.out.println("4. Resultado de convertir un String a int");
+            System.out.println("5. Resultado de (int) (char) (byte) -1");
+            System.out.println("6. Resultado del número 65 a un char");
+            System.out.println("7. Resultado de asignar un long a un int sin casteo explícito");
+            System.out.println("8. Resultado de convertir un float a un String");
+            System.out.println("9. Resultado de de (int) Math.round(3.7)");
+            System.out.println("10. Resultado de convertir byte b = 50 a short");
+            System.out.println("0. Volver al menú principal");
+            System.out.print("Seleccione una opción: ");
+
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("El resultado de la operacion es: " + Main.calcularResultadoDex());
+                    break;
+                case 2:
+                    System.out.println("El valor ASCII de A es: " + Main.calcularValorAscii());
+                    break;
+                case 3:
+                    System.out.println("El valor que se imprimira es: " + Main.imprimirValorDei());
+                    break;
+                case 4:
+                    System.out.println("El valor que se imprimira es: " + Main.convertirStringAint());
+                    break;
+                case 5:
+                    System.out.println("El resultado es: " + Main.resultadoDeConvercion());
+                    break;
+                case 6:
+                    System.out.println("El resultado de (int) (char) (byte) -1 es: " + Main.convertirNumeroEnChar());
+                    break;
+                case 7:
+                    System.out.println("¿Qué sucede si intentas asignar un long a un int sin casteo explícito?: " + Main.asignarLongAInt());
+                    break;
+                case 8:
+                    System.out.println("float 3.14 a String: " + Main.convertirFloatAString());
+                    break;
+                case 9:
+                    System.out.println("Resultado de (int) Math.round(3.7): " + Main.resultadoDeIntConMathRound());
+                    break;
+                case 10:
+                    System.out.println("Resultado byte 50 a short:: " + Main.convertirByteAShort());
+                    break;
+                case 0:
+                    return; // Volver al menú principal
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
+            }
+            System.out.println();
+        } while (option != 0);
+    }
+
+    private static void submenuOperadoresDeAsignacion(Scanner scanner) {
+        int option;
+        do {
+            System.out.println("Submenú Operadores de Asignación:");
+            System.out.println("1. Resultado del valor de x despues de x += 5");
+            System.out.println("2. Resultado de simplificar la expresión usando un operador de asignación compuesta");
+            System.out.println("3. Resultado del valor de a despues de a %= 4");
+            System.out.println("4. Resultado de una expresión equivalente a x = x + y");
+            System.out.println("5. Resultado de x ^= 2;");
+            System.out.println("0. Volver al menú principal");
+            System.out.print("Seleccione una opción: ");
+
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("El valor de X es: " + Main.valorX());
+                    break;
+                case 2:
+                    System.out.println("El valor de Y es: " + Main.valorY());
+                    break;
+                case 3:
+                    System.out.println("El valor de a es: " + Main.valorDea());
+                    break;
+                case 4:
+                    System.out.println("El valor de X es: " + Main.valorDeXUsandoOperadorDeAsignacionCompuesta());
+                    break;
+                case 5:
+                    System.out.println("El resultado de la operacion es: " + Main.resultadoOperacion());
+                    break;
+                case 0:
+                    return; // Volver al menú principal
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
+            }
+            System.out.println();
+        } while (option != 0);
+    }
+
+    private static void submenuOperadoresDeIncrementoYDecremento(Scanner scanner) {
+        int option;
+        do {
+            System.out.println("Submenu Operadores de Incremento y Decremento:");
+            System.out.println("1. Resultado del valor de x despues de x = 5 y Y despues de y = ++x");
+            System.out.println("2. ¿Qué se imprimirá en la consola?  int a = 5");
+            System.out.println("3. Resultado de un código que use el operador de decremento para disminuir una variable count en 1");
+            System.out.println("4. Resultado de la diferencia entre ++i y i++");
+            System.out.println("5. Resultado de x despues de x = x++");
+            System.out.println("0. Volver al menú principal");
+            System.out.print("Seleccione una opción: ");
+
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("El resultado de Y es: " + Main.resultadoDeXyY());
+                    break;
+                case 2:
+                    System.out.println("El resultado de a++ es: " + Main.resultadoDeAMas() + "  El resultado de a es: " + Main.resultadoDeA());
+                    break;
+                case 3:
+                    System.out.println("El resultado del decremento es: " + Main.resultadoDeDecremento());
+                    break;
+                case 4:
+                    System.out.println("El valor de ++i es :" + Main.incrementoEnPrefijo() + " el cual aumento 2 mas en cambio el i++ es : " + Main.incrementoEnSufijo() + " el cual aumento solo uno mas");
+                    break;
+                case 5:
+                    System.out.println("El valor que tiene x es : " + Main.valordex());
+                    break;
+                case 0:
+                    return; // Volver al menú principal
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
+            }
+            System.out.println();
+        } while (option != 0);
+    }
+
+    private static void submenuEjerciciosCombinados(Scanner scanner) {
+        int option;
+        do {
+            System.out.println("Submenú Ejercicios Combinados:");
+            System.out.println("1. Resultado de la siguiente expresion i += ++i + i++ + ++i");
+            System.out.println("2. Resultado de convertir un double a un int, y luego incrementa el resultado en 1 usando el operador de postincremento");
+            System.out.println("3. Resultado de imprimir en la consola i--");
+            System.out.println("4. Resultado de la expresión que use casteo y un operador de asignación compuesta para dividir un int por 2.0 y asignar el resultado a una variable double.");
+            System.out.println("5. Resultado de c despues de c += 32");
+            System.out.println("6. Resultado de convertir un long a un short, y luego usa un operador de asignación compuesta para multiplicarlo por 3");
+            System.out.println("7. Resultado del valor de x despues de x += (x++) + (++x)");
+            System.out.println("8. Resultado de la expresión que use casteo para convertir un float a un byte, y luego usa el operador de predecremento");
+            System.out.println("9. Resultado de b despues de byte b = (byte)");
+            System.out.println("10. Resultado de la expresión que combine casteo, un operador de asignación compuesta y un operador de incremento para convertir un double a un int, sumarle 5 y luego incrementarlo en 1");
+            System.out.println("0. Volver al menú principal");
+            System.out.print("Seleccione una opción: ");
+
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("El resultado de i es: " + Main.resultadoDeI());
+                    break;
+                case 2:
+                    System.out.println("El resultado de convertir el Double en Int y luego incrementar es: " + Main.convertirDoubleEnIntEincrementar());
+                    break;
+                case 3:
+                    System.out.println("El valor de i es : " + Main.valorDei());
+                    break;
+                case 4:
+                    System.out.println("Resultado: " + Main.valorDeC());
+                    break;
+                case 5:
+                    System.out.println("El resultado de la operacion es: " + Main.resultadoDeC());
+                    break;
+                case 6:
+                    System.out.println("El resultado de convertir long en short y luego multiplicarlo por 3 es: " + Main.convertirLongEnShort());
+                    break;
+                case 7:
+                    System.out.println("El resultado de X en la operacion es: " + Main.resultadoOpercaionX());
+                    break;
+                case 8:
+                    System.out.println("El resultado de convertir Float en Byte y luego hacer e predecremento es: " + Main.resultadoDelPredecremento());
+                    break;
+                case 9:
+                    System.out.println("El resultado de b en la operacion es: " + Main.resultadoOperacionb());
+                    break;
+                case 10:
+                    System.out.println("El resultado es: " + Main.resultadoDeIncrementarleEn1());
+                    break;
+                case 0:
+                    return; // Volver al menú principal
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
+            }
+            System.out.println();
+        } while (option != 0);
+    }
 }
